23f3d2f2e2d9a743fb8c4407d14e2480bfa73fd2
georgekoul26 <georgekoul@gmail.com>
2021-01-23 21:42:43 -0600

    Updated

--- Basic_Stock_Recommender/stock_recommendation_V2_WIP.py
+++ Basic_Stock_Recommender/stock_recommendation_V2_WIP.py
@@ -6,52 +6,74 @@
 #Create recommendation function which calculates the percent difference and provides a recommendation on whether to buy/sell/hold
 
 def recommend():
-    original_price = float(input("Original Price:"))
-    current_price = float(input("Current Price:"))
-    balance = float(input("Account Balance:"))
+    original_price = float(input("Original Price ($):"))
+    current_price = float(input("Current Price ($):"))
+    balance = float(input("Account Balance ($):"))
     threshold_to_buy = -20
     threshold_to_sell = 20
     difference = current_price - original_price
     percent_difference = difference / original_price * 100
     if percent_difference >= threshold_to_sell:
-        balance += current_price
         recommendation = "Sell"
     elif percent_difference <= threshold_to_buy:
         if balance >= current_price:
-            balance -= current_price
             recommendation = "Buy"
-        else: print ("Insufficient funds")
+        else:
+            recommendation = "You have insufficent funds."
     else: recommendation = "Hold"
     print (recommendation)
-    return recommendation, balance, current_price
+    return recommendation, current_price, balance
 
 #Request number of shares to buy/sell and define execute function to execute trade and print new "share quantity" and "account balance"
 
 
 def execute():
     recommendation, current_price, balance = recommend()
-    shares_owned = int(input("Number of shares owned:"))
-    if recommendation == "Sell": 
-        sell_order_quantitiy = int(input("How many shares do you want to sell?"))
-        if sell_order_quantitiy > shares_owned:
-            print ("You do not own that many shares.  Let's try again.")
-            print ("How many shares do you want to sell?")
-        elif sell_order_quantitiy < 0:
-            print ("This input is not valid.  Let's try again.")
-            print ("How many shares do you want to sell?")
-        elif sell_order_quantitiy == 0:
-            print ("Confirmed.  You want to hold your position.")
-        else:
-            balance += sell_order_quantitiy * current_price
-            new_shares_owned = shares_owned - sell_order_quantitiy
-            print (f"Your new balance is ${balance}")
-            print (f"Your now own {new_shares_owned} shares.")
+    if recommendation == "Sell":
+        shares_owned = int(input("Number of shares owned:")) 
+        while True:
+            try: 
+                sell_order_quantitiy = int(input("How many shares do you want to sell?"))
+            except ValueError:
+                print ("This input is not valid.  Let's try again.")
+                continue
+            if sell_order_quantitiy > shares_owned:
+                print ("You do not own that many shares.  Let's try again.")
+                continue
+            elif sell_order_quantitiy < 0:
+                print ("This input is not valid.  Let's try again.")
+                continue
+            elif sell_order_quantitiy == 0:
+                print ("Confirmed.  You want to hold your position.")
+                break
+            else:
+                balance += sell_order_quantitiy * current_price
+                new_shares_owned = shares_owned - sell_order_quantitiy
+                print (f"Your new balance is ${balance}")
+                print (f"You now own {new_shares_owned} shares.")
+                break
     if recommendation == "Buy":
-        buy_order_quantitiy = int(input("How many shares do you want to buy?"))
-        if buy_order_quantitiy * current_price > balance:
-            print ("You have and inadequate balance for that purchase.  Let's try again.")
-            print ("How many shares do you want to buy?")
-        else: print ("Placeholder")
+        shares_owned = int(input("Number of shares owned:"))
+        while True:
+            try:
+                buy_order_quantitiy = int(input("How many shares do you want to buy?"))
+            except ValueError:
+                print ("This input is not valid.  Let's try again.")
+                continue  
+            if buy_order_quantitiy * current_price > balance:
+                print ("You have and inadequate balance for that purchase.  Let's try again.")
+                continue
+            elif buy_order_quantitiy == 0:
+                print ("Confirmed.  You do not want to buy any shares")
+                break
+            else:
+                balance -= buy_order_quantitiy * current_price
+                new_shares_owned = shares_owned + buy_order_quantitiy
+                print (f"Your new balance is ${balance}")
+                print (f"Your now own {new_shares_owned} shares.")
+                break
+    else: return
+
 
 #Call functions using main function
 
